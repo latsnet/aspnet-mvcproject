40ace1ac61c7d56c92594a4cd26853631acf396f
Luiz Augusto Tavares da Silveira <latsnet@gmail.com>
2022-11-07 14:30:47 -0300

    Using Data Annotations

--- Models/Snack.cs
+++ Models/Snack.cs
@@ -1,14 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace mvc_project.Models;
 
+[Table("Lanches")]
 public class Snack
 {
+
     public int SnackId { get; set; }
 
+    [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+    [Display(Name = "Nome do Lanche")]
     public string SnackName { get; set; }
 
+    [Required]
+    [Display(Name = "Descrição do Lanche")]
+    [MinLength(20)]
+    [MaxLength(200)]
     public string ShortDescription { get; set; }
 
+    [Required(ErrorMessage = "A descrição completa deve ser informada")]
+    [Display(Name = "Descrição Completa")]
     public string DetailDescription { get; set; }
+
 
     public decimal Price { get; set; }
 
@@ -23,5 +37,8 @@ public class Snack
     public int CategoryId { get; set; }
 
     public virtual Category Category { get; set; }
+
+    [NotMapped]
+    public DateTime CreatedDate { get; set; }
 
 }
--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 How to run in Development mode:
 
     export ASPNETCORE_ENVIRONMENT=Development
+
+Migrations with NET CLI
+
+    dotnet tool install --global dotnet-ef
+
+Create Migration
+
+    dotnet ef migrations add <Migration_Name> [options]
+
+Apply Migration
+
+    dotnet ef database update [options]
+
+Remove Migration
+
+    dotnet ef migrations remove
