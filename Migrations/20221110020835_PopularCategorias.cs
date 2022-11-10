aa7e8e4952d7e9ba6c3d2e39c0d991b751822810
Luiz Augusto Tavares da Silveira <latsnet@gmail.com>
2022-11-09 23:17:06 -0300

    Populating table Categories

---
+++ Migrations/20221110020835_PopularCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mvc_project.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO Categories (CategoryName, Description) Values 
+                                ('Normal', 'Lanche feito com ingredientes normais')");
+
+            migrationBuilder.Sql(@"INSERT INTO Categories (CategoryName, Description) Values 
+                                ('Natural', 'Lanche feito com ingredientes integrais e naturais')");
+                            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM Categories");
+        }
+    }
+}
