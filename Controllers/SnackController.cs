bd7653cd00d9a8b82b0cfa95988776112d9c8e49
Luiz Augusto Tavares da Silveira <latsnet@gmail.com>
2022-11-10 23:45:57 -0300

    Creating a new Controller for Snack List

---
+++ Controllers/SnackController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvc_project.Models;
+using mvc_project.Repositories.Interfaces;
+
+namespace mvc_project.Controllers;
+
+public class SnackController : Controller
+{
+
+    private readonly ILogger<SnackController> _logger;
+    private readonly ISnackRepository _snackRepository;
+
+    public SnackController(ILogger<SnackController> logger, ISnackRepository snackRepository) 
+    {
+        _logger = logger;
+        _snackRepository = snackRepository;
+    }    
+    
+    public IActionResult List() 
+    {
+        var snacks = _snackRepository.Snacks;
+        return View(snacks);
+    }
+
+}
